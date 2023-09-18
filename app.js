5186a0298c5f6f23fa83bd19fbf0062391891566
rahulfordev <rahulforsms@gmail.com>
2023-09-18 18:03:15 +0600

    carousel design

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".testi-content").slick({
+    autoplay: false,
+    dots: true,
+
+    prevArrow:
+      '<button type="button" class="slick-prev"><i class="fa-solid fa-angle-left"></i></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><i class="fa-solid fa-angle-right"></i></button>',
+  });
+});
